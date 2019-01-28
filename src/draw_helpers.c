34657be8c09b605f3aacece9f304b9af3d234698
jeffersoncity <jeffersoncity95@gmail.com>
2019-01-28 21:26:15 +0300

    a

--- src/draw_helpers.c
+++ src/draw_helpers.c
@@ -6,7 +6,7 @@
 /*   By: cmelara- <cmelara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 21:46:59 by cmelara-          #+#    #+#             */
-/*   Updated: 2019/01/28 21:02:15 by cmelara-         ###   ########.fr       */
+/*   Updated: 2019/01/28 21:20:09 by cmelara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ void	draw_column(t_engine *engine, int x, t_col y_column, t_ray *ray)
 {
 	SDL_Rect	*tex_rect;
 	SDL_Rect	win_rect;
+	int			temp;
 
+	temp = (int)((1.0f - (double)abs(y_column.end - y_column.start)
+				/ (double)ray->wall_height) * 64);
 	tex_rect = ft_memalloc(sizeof(SDL_Rect));
 	tex_rect->w = 1;
-	tex_rect->h = TEX_H;
+	tex_rect->h = TEX_H - temp;
 	//printf("tex_x: %d\n", ray->tex_x);
 	tex_rect->x =  TEX_W * (ray->wall_id - 1) + ray->tex_x;
-	tex_rect->y = 0;//(int)((1.0f - (double)abs(y_column.end - y_column.start) / (double)ray->wall_height) * 64);
+	tex_rect->y = temp;
 
 	win_rect.w = 1;
 	win_rect.h = abs(y_column.end - y_column.start);
